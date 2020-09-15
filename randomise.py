0df66d3c4ee115d04fdc264f883d3a36a4bc1e0e
Simon Metson <simonmetson@googlemail.com>
2020-09-15 13:23:52 +0100

    bounding boxes

--- randomise.py
+++ randomise.py
@@ -9,29 +9,35 @@ from PIL import Image
 
 def join_modules(modules):
     image = False
+    all_coords = []
     while len(modules):
         if image:
-            image = get_concat_h_resize(image, modules.pop())
+            image, coords = get_concat_h_resize(image, modules.pop())
         else:
-            image = get_concat_h_resize(modules.pop(), modules.pop())
-    return image
+            image, coords = get_concat_h_resize(modules.pop(), modules.pop())
+        all_coords.append(coords)
+    return image, all_coords
 
-# Cribbed from https://note.nkmk.me/en/python-pillow-concat-images/
+
 def get_concat_h_resize(im1, im2, resample=Image.BICUBIC, resize_big_image=True):
+    # Cribbed from https://note.nkmk.me/en/python-pillow-concat-images/
     if im1.height == im2.height:
         _im1 = im1
         _im2 = im2
     elif (((im1.height > im2.height) and resize_big_image) or
           ((im1.height < im2.height) and not resize_big_image)):
-        _im1 = im1.resize((int(im1.width * im2.height / im1.height), im2.height), resample=resample)
+        _im1 = im1.resize(
+            (int(im1.width * im2.height / im1.height), im2.height), resample=resample)
         _im2 = im2
     else:
         _im1 = im1
-        _im2 = im2.resize((int(im2.width * im1.height / im2.height), im1.height), resample=resample)
+        _im2 = im2.resize(
+            (int(im2.width * im1.height / im2.height), im1.height), resample=resample)
     dst = Image.new('RGB', (_im1.width + _im2.width, _im1.height))
     dst.paste(_im1, (0, 0))
     dst.paste(_im2, (_im1.width, 0))
-    return dst
+    return dst, (dst.width, dst.height, _im1.width, _im2.width)
+
 
 def pick_modules_from_dir(data_dir='data', count=1):
     data = list(pathlib.Path(data_dir).glob('./*.jpg'))
@@ -42,6 +48,7 @@ def pick_modules_from_dir(data_dir='data', count=1):
         result.append(Image.open(p.as_posix()))
         i += 1
     return result
+
 
 def pick_modules_from_data(jsonfile='data/modules_page_1.json', count=1):
     with open(jsonfile) as f:
@@ -60,9 +67,11 @@ def pick_modules_from_data(jsonfile='data/modules_page_1.json', count=1):
                     i += 1
     return result
 
+
 if __name__ == "__main__":
     modules = pick_modules_from_data(count=5)
-    image = join_modules([m["image"] for m in modules.values()])
+    image, all_coords = join_modules([m["image"] for m in modules.values()])
+    print(all_coords)
     hash = hashlib.sha256()
     for i in [m.encode('utf-8') for m in modules]:
         hash.update(bytes(i))
